b37eb1c3bf7d607f8d2396cc8bfb90957b280fe0
Andres Serrano <andres.srg@gmail.com>
2017-09-23 14:25:51 -0500

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
